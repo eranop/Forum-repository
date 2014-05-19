f6f0dc70b562a7a0485416b82aeba8512f8e5a29
elitzura <aviadelitzur@gmail.com>
2014-05-19 18:33:46 +0300

    local RMI finish

--- Forum/src/rmi/Constants.java
+++ Forum/src/rmi/Constants.java
@@ -1,27 +1,25 @@
-package rmi;
 
+package rmi;
+ 
 public class Constants {
-	private static String RMI_ID = "RMI!";
-	private static int RMI_PORT = 223;
-	private static boolean requestFlag=false;
-	public static int getPortIncrease(){
-		RMI_PORT++;
-		return RMI_PORT;
-	}
-	public static String getIDIncrease(){
-		RMI_ID = RMI_ID.concat("*");
-		return RMI_ID;
-	}
-	public static String getID(){
-		return RMI_ID;
-	}
-	public static int getPort(){
-		return RMI_PORT;
-	}
-	public static boolean getFlag(){
-		return requestFlag;
-	}
-	public static void setFlag(boolean b){
-		requestFlag = b;
-	}
+        private static String RMI_ID = "RMI!";
+        private static int RMI_PORT = 200;
+        public static int getPortIncrease(){
+                RMI_PORT++;
+                return RMI_PORT;
+        }
+        public static String getIDIncrease(){
+                RMI_ID = RMI_ID.concat("*");
+                return RMI_ID;
+        }
+        public static String getID(){
+                return RMI_ID;
+        }
+        public static int getPort(){
+                return RMI_PORT;
+        }
+        public static void setPort(int i){
+                RMI_PORT=i;
+        }
+       
 }
--- Forum/src/rmi/RMIclient.java
+++ Forum/src/rmi/RMIclient.java
@@ -1,28 +1,25 @@
-package rmi;
 
+package rmi;
+ 
+import java.net.ServerSocket;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-
+ 
 public class RMIclient {
-
-	/**
-	 * @param args
-	 * @throws RemoteException 
-	 * @throws NotBoundException 
-	 * @throws InterruptedException 
-	 */
-	public static void main(String[] args) throws RemoteException, NotBoundException, InterruptedException {
-		Constants.setFlag(true);
-		Thread.sleep(5000);
-		int tmp1 = Constants.getPortIncrease();
-		String tmp2 = Constants.getIDIncrease();
-		System.out.print("started new client on port: "+tmp1);
-		System.out.print("started new client on id: "+tmp2);
-		Registry reg = LocateRegistry.getRegistry(null, tmp1);
-		remoteInterface remote = (remoteInterface) reg.lookup(tmp2);
-		System.out.println(remote.login("somename", "pass"));
-	}
-
+        /**
+         * @param args
+         * @throws RemoteException
+         * @throws NotBoundException
+         * @throws InterruptedException
+         */
+        public static void main(String[] args) throws RemoteException, NotBoundException, InterruptedException {
+                Registry reg = LocateRegistry.getRegistry(null, Constants.getPortIncrease());
+                remoteInterface remote = (remoteInterface) reg.lookup(Constants.getID());
+                System.out.println(remote.login("somename", "pass"));
+        }
+ 
 }
+ 
+ 
