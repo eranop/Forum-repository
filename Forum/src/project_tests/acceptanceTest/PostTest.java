33661e40a2ac33b3ce37d0794ec6315d17365672
elitzura <aviadelitzur@gmail.com>
2014-05-23 14:05:18 +0300

    more tests

--- Forum/src/project_tests/acceptanceTest/PostTest.java
+++ Forum/src/project_tests/acceptanceTest/PostTest.java
@@ -1,7 +1,10 @@
 package project_tests.acceptanceTest;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class PostTest extends ForumSiteTest{
 
@@ -9,16 +12,61 @@ public class PostTest extends ForumSiteTest{
 		super();
 	}
 	@Before
-	 public void initialize() {
+	 public void set_up() {
 		getRegularConnection();
 		_site.exitForum();
 		_site.enterForum("food1");
+		_site.login("aviad", "aviad");
+		_site.enterSubforum("milk1");
 	}
 	@After
 	public void after() {
 		_site.logout(); 
 		_site.exitForum();
   }
-
+	
+	
+	@Test
+	public void simplePostTest(){
+		
+	}
+	@Test
+	public void postWithMissingFieldTest(){
+		
+	}
+	@Test
+	public void postWithLongTextTest(){
+		
+	}
+	
+	
+	@Test
+	public void simpleResponseTest(){
+		
+	}
+	@Test
+	public void responseToNoExistPostTest(){
+		
+	}
+	@Test
+	public void selfResponseTest(){
+		
+	}	
+	@Test
+	public void postPreconditionsTest(){
+		/*
+		 * must be member
+		 * in sub-forum
+		 */
+	}	
+	@Test
+	public void responsePreconditionsTest(){
+		/*
+		 * must be member 
+		 * in post
+		 */
+	}	
+	
+	
 	
 }
