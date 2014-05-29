33260eb4311826129cf118078ee73da3fd436140
tzvikaShapira <tzshapira@gmail.com>
2014-05-29 16:11:08 +0300

    aa

---
+++ Forum/src/allcode/DataBaseInit.java
@@ -0,0 +1,28 @@
+package allcode;
+
+import org.hibernate.Session;  
+import org.hibernate.SessionFactory;  
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;  
+import org.hibernate.service.ServiceRegistry;  
+
+
+
+public class DataBaseInit {
+	
+	public static Configuration configuration;
+	public static ServiceRegistry serviceRegistry;
+	public static SessionFactory sf;
+
+	public DataBaseInit(){
+
+	}
+	public static void initialize(){
+		configuration=new Configuration();  
+		configuration.configure();  
+		serviceRegistry = 
+				new StandardServiceRegistryBuilder().applySettings( configuration.getProperties() ).build(); 
+
+		sf = configuration.buildSessionFactory(serviceRegistry);
+	}
+}
