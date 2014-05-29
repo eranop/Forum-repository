ed838de2e300375e659ce0bfb0108250921ba95c
tzvikaShapira <tzshapira@gmail.com>
2014-05-29 15:28:50 +0300

    aaa

---
+++ Forum/src/allcode/DataBaseInit.java
@@ -0,0 +1,29 @@
+package allcode;
+
+import org.hibernate.Session;  
+import org.hibernate.SessionFactory;  
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;  
+import org.hibernate.service.ServiceRegistry;  
+
+
+
+public class DataBaseInit {
+	
+	public static Configuration configuration;
+	public static ServiceRegistry serviceRegistry;
+	public static SessionFactory sf;
+
+	public DataBaseInit(){
+
+	}
+	public static void initialize(){
+		configuration=new Configuration();  
+		configuration.configure(); 
+		System.out.println("im initializing!");
+		serviceRegistry = 
+				new StandardServiceRegistryBuilder().applySettings( configuration.getProperties() ).build(); 
+
+		sf = configuration.buildSessionFactory(serviceRegistry);
+	}
+}
--- Forum/src/allcode/SubForum.java
+++ Forum/src/allcode/SubForum.java
@@ -8,6 +8,20 @@ import services.Complain;
 import services.Response;
 import services.report;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapKey;
+import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="sub-forum")
 public class SubForum {
 	
 	private int _msgCounter;					//will count the messages and arrange thier indexes.
