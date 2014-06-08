039b2ab91211613fcb265aa9c5e75fb5adbb128b
elitzura <aviadelitzur@gmail.com>
2014-06-08 14:53:24 +0300

    dsf

--- Forum/src/allcode/InitializeSystem.java
+++ Forum/src/allcode/InitializeSystem.java
@@ -1,11 +1,15 @@
 package allcode;
 
+import services.Logger2;
+
 public class InitializeSystem {
 
 	public static SiteManager init(String superAdminName, String password, String email){
 
 		SiteManager sm= new SiteManager(superAdminName, password, email);
-		
+		//Logger2.initLogSystem();
+		//Logger2.initLogUser();
+
 		return sm;
 	}	
 }
